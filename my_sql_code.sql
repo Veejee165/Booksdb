34bd2f4369c7eaa8dbd7324c1139611e09c5eb44
Veejee <vedant165gupta@gmail.com>
2023-05-05 22:33:24 +0400

    Pre pres commit

--- my_sql_code.sql
+++ my_sql_code.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS books;
-DROP TABLE IF EXISTS bookowner;
+DROP TABLE IF EXISTS nocount;
 
 CREATE TABLE IF NOT EXISTS books (
   year INTEGER,
@@ -11,12 +11,12 @@ CREATE TABLE IF NOT EXISTS books (
   PRIMARY KEY(year, course, id)
 );
 
-CREATE TABLE IF NOT EXISTS bookowner(
+CREATE TABLE IF NOT EXISTS nocount(
   cno INTEGER,
   number INTEGER,
   id CHAR(13),
   PRIMARY KEY(id)
-)
+);
 
 CREATE TRIGGER IF NOT EXISTS check_number
 BEFORE INSERT ON books
@@ -26,23 +26,6 @@ BEGIN
   SELECT RAISE(ABORT, 'Number must be greater than zero');
 END;
 
-CREATE TRIGGER IF NOT EXISTS insert_nocount
-AFTER INSERT ON books
-FOR EACH ROW
-BEGIN
-  -- check if the id is present in the nocount table
-  SELECT COUNT(*) FROM nocount WHERE id = NEW.id;
-  IF (SELECT changes() = 0)
-  THEN
-    -- if the id is not present, insert a new row into nocount
-    INSERT INTO nocount (id, number, cno) VALUES (NEW.id, 0, NEW.number);
-  ELSE
-    -- if the id is present, update the count in nocount
-    UPDATE nocount SET number = number + 1 WHERE id = NEW.id;
-  END IF;
-END;
-
-
 CREATE TRIGGER IF NOT EXISTS check_count
 BEFORE INSERT ON books
 FOR EACH ROW
@@ -51,4 +34,18 @@ BEGIN
   SELECT RAISE(ABORT, 'Maximum 6 books allowed per student');
 END;
 
+CREATE TRIGGER IF NOT EXISTS insert_nocount
+AFTER INSERT ON books
+WHEN (SELECT COUNT(*) FROM nocount WHERE id = NEW.id) = 0
+BEGIN
+  -- if the id is not present, insert a new row into nocount
+  INSERT INTO nocount (id, number, cno) VALUES (NEW.id, 0, NEW.number);
+END;
 
+CREATE TRIGGER IF NOT EXISTS update_nocount
+AFTER INSERT ON books
+WHEN (SELECT COUNT(*) FROM nocount WHERE id = NEW.id) > 0
+BEGIN
+  -- if the id is present, update the count in nocount
+  UPDATE nocount SET number = number + 1 WHERE id = NEW.id;
+END;
